f858d3007824723f5592dc6b288ae61bdb2b9c64
Abhishek Maheshwari <abhishek27082000@gmail.com>
2020-12-22 01:25:15 +0530

    Added Video Trailers

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "1f80418367d4a5cb83d39a16810870fd";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchActionMovies: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+}
+
+export default requests;
